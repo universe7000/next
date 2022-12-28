de52ffa197ebe7c9ecf2af6db789e2c7fb9688d7
universla84 <121558922+universla84@users.noreply.github.com>
2022-12-28 07:19:16 -0600

    Add files via upload

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+import { Footer } from '../Footer'
+
+export default function Page() {
+    return (
+        <>
+            <Link href="/about">about</Link>
+            <Link href="/contact">contact</Link>
+            <Footer />
+        </>
+    )
+}
